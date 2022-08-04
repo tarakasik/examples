dd5484ab10ac4f1a546db5bcf1b43df2f345e156
Stanislav Volokhov <lamantina123@gmail.com>
2022-08-04 19:52:29 +0400

    add Example005_Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользоваателя: ");
+string username = Console.ReadLine();
+
+if(usernane == "Маша")
+
+    Console.WriteLine("Ура, это же МАША");
+
+else
+
+    Console.Write("Привет");
+    Console.WriteLine(username);
+
